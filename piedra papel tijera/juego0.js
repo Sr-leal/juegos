4f177f771d6b5f3407e63db0f31e40f9751d5193
Sr-leal <82217599+Sr-leal@users.noreply.github.com>
2021-04-23 14:34:53 +0200

    Add files via upload

--- piedra papel tijera/juego0.js
+++ piedra papel tijera/juego0.js
@@ -27,57 +27,73 @@ function quest() {
 		alert("El script de Leal ha escogido"+" "+maquina);
 		if (maquina == usuario) {
 				empate();
-			} else {
-				if ((maquina == "piedra") && (usuario == "tijera")) {
+			}
+			 	if (usuario == "piedra") {
+					if (maquina == "tijera") {
+						victoria();
+						contador++
+						comprobarc();
+					} else if (maquina == "papel") {
+						derrota();
+						contador--
+						comprobarc();
+					}
+			 	}
+				if (usuario == "papel") {
+					if (maquina == "piedra") {
+						victoria();
+						contador++
+						comprobarc();
+					} else if (maquina == "tijera") {
+						derrota();
+						contador--
+						comprobarc();
+					}
+			}
+			if (usuario == "tijera") {
+				if (maquina == "papel") {
 					victoria();
 					contador++
 					comprobarc();
-			 	} else if ((maquina == "papel") && (usuario == "piedra")) {
-					victoria();
-					contador++
-					comprobarc();
-			 	} else if ((maquina == "tijera") && (usuario == "papel")) {
-					victoria();
-					contador++
-					comprobarc();
-			 	} else {
+				} else if (maquina == "piedra") {
 					derrota();
 					contador--
 					comprobarc();
-			 	}
-			}
-   }
+			  }
+		}
+	} else {
+		alert("Escoge entre piedra, papel o tijera")
+	}
  }
+
 function empate() {
 	var imgempate = document.getElementById("img1").src="https://media3.giphy.com/media/WsRFoTvgBjA7zE5EZi/giphy.gif";
 	removebt();
-	window.setTimeout("refrescarpax()",3000);
+	window.setInterval("refrescarpax()",3000);
 }
 function victoria() {
 	var imgvictoria = document.getElementById("img1").src="https://img.wattpad.com/cc352d4796366ce38ba76e18c826ede42e466ce3/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f41625151337932436f75796937673d3d2d3433363537383436332e313463656465353831376261316137663434333435313437323736372e676966";
 	removebt();
-	window.setTimeout("refrescarpax()",3000);
+	window.setInterval("refrescarpax()",3000);
 ;
 }
 function derrota() {
 	var imgderrota = document.getElementById("img1").src="https://img.wattpad.com/94ad44e36139f25cef4402a45db9a9999f742d1c/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f5673646c757834706c6f415649513d3d2d3235333935383132372e313434633466306132376666336331362e676966";
 	removebt();
-	window.setTimeout("refrescarpax()",3000);
+	window.setInterval("refrescarpax()",3000);
 }
  function refrescarpax() {
   document.getElementById("img1").src="https://www.omega-level.net/wp-content/uploads/2013/09/fight.gif";
 	boton.style.display = "block"
-	boton2.style.display = "block"
  }
 function removebt() {
 	boton.style.display = "none"
-	boton2.style.display = "none"
 }
 function comprobarc(){
       if (contador === 0){
         alert('Te has quedado sin vidas </3');
 				contador === 1
-      } else if (contador === 3) {
+      } else if (contador == 3) {
         alert('Enhorabuena has conseguido <3 <3 <3 ');
       }
 		}
